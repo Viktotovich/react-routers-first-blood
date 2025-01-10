473fc3ded357be2cc404a241e49430caff1a468c
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-01-10 18:41:26 +0400

    Error boundaries and dynamic routes testing

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./components/ErrorPage.jsx";
 
 import Profile from "./components/profile.jsx";
-import Spinach from "./components/Spinach.jsx";
-import Popeye from "./components/Popeye.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
   },
   {
-    path: "profile",
+    path: "profile/:name",
     element: <Profile />,
-    children: [
-      { path: "spinach", element: <Spinach /> },
-      { path: "popeye", element: <Popeye /> },
-    ],
   },
 ]);
 
